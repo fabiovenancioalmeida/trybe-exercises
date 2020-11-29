afcf09f0d34a9276d220ceeb99d2231cb89c598f
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2020-11-29 07:18:23 -0300

    limpa branch'

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/divisao.js
+++
@@ -1,6 +0,0 @@
-let a = 30;
-let b = 5;
-
-const result = a / b;
-
-console.log(`O resultado da divisão é ${result}`);
--- exercises/introducao-a-javascript-e-logica-de-programacao.1/modulo.js
+++
@@ -1,6 +0,0 @@
-let a = 32;
-let b = 7;
-
-const result = a % b;
-
-console.log(`O resultado do módulo é ${result}`);
