69cde846c4ed899c015a4f7855042906e2c8cad8
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2020-11-29 07:26:50 -0300

    Limpa branch

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/adicao.js
+++
@@ -1,6 +0,0 @@
-let a = 7;
-let b = 5;
-
-const result = a + b;
-
-console.log(`O resultado adição é ${result}`);
--- exercises/introducao-a-javascript-e-logica-de-programacao.1/substracao.js
+++
@@ -1,6 +0,0 @@
-let a = 7;
-let b = 5;
-
-const result = a - b;
-
-console.log(`O resultado da subtração é ${result}`);
